f9ef105b56f6c922129d4c78301601c97f3336dd
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-03-22 10:26:22 +0300

    cleanup

--- src/pages/api/getBooks.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "../../utils/dbConnect"
-import Book from "../../models/bookModel"
-
-dbConnect()
-
-export default async function handler(req, res) {
-  await Book.find().then((foundBooks) => res.json(foundBooks))
-}
